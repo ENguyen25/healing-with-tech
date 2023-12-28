b9213e55c5ec09feed07fc7059c7f08843f84d72
Evelyn Nguyen <enguyen6208@gmail.com>
2023-12-27 18:39:03 -0800

    added footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './Footer.css';
 
 const Footer = () => {
   return (
-    <div></div>
+    <section className='footer'>
+      <div className="footer-links">
+        <Link to='/about'>About</Link>
+        <Link to='/contact'>Contact</Link>
+        <Link to='/coding'>Coding</Link>
+        <Link to='/career'>Career</Link>
+        <Link to='/self-care'>Self-Care</Link>
+      </div>
+      <div className="social-media-links"></div>
+      <p className="copyright">&copy; 2023 <span>Evelyn Nguyen</span>, All Rights Reserved</p>
+    </section>
   )
 }
 
